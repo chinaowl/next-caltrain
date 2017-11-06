f14649dde710dd8739c8bd4d91fb92f9f80bbb3b
China Wang <chinana523@gmail.com>
2017-11-05 18:26:32 -0800

    update django database settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'next_caltrain',
+        'USER': 'next_caltrain_user',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
